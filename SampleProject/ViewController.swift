f5f8a5d8a94f4ed9922a3282325278c42b4167ec
Heiko von Wegerer <prihei@web.de>
2020-05-08 13:56:22 +0200

    Initial commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Heiko von Wegerer on 08.05.20.
+//  Copyright © 2020 Herised. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
