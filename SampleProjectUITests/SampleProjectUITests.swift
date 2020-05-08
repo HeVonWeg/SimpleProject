db5c093485527522b968713b3a3f1add1e4aa29b
Heiko von Wegerer <prihei@web.de>
2020-05-08 17:04:17 +0200

    XCUITest failed

--- SampleProjectUITests/SampleProjectUITests.swift
+++ SampleProjectUITests/SampleProjectUITests.swift
@@ -27,6 +27,8 @@ class SampleProjectUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        XCTAssertTrue(false)
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
